d2030714b8809fdab25214415f000b92899b7284
yamamoto@kamome-e.com <yamamoto@kamome-e.com>
2017-10-30 10:39:51 +0900

    Password encryption for JDBC authentication

--- openam-authentication/openam-auth-jdbc/src/main/java/com/sun/identity/authentication/modules/jdbc/EncryptedTextTransform.java
+++ openam-authentication/openam-auth-jdbc/src/main/java/com/sun/identity/authentication/modules/jdbc/EncryptedTextTransform.java
@@ -1,26 +1,28 @@
 package com.sun.identity.authentication.modules.jdbc;
 
-import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 
 import com.iplanet.services.util.OpenLDAPEncryption;
-   
+
 /**
  * パスワード暗号化ロジック OpenLDAP方式(SSHA)
  * かもめエンジニアリング 山本 2017.11.17
  */
 public class EncryptedTextTransform implements JDBCPasswordSyntaxTransform  {
-	
-	private static final String ALG_SHA1 = "SHA-1";     // ハッシュ化のアルゴリズム：SHA-1
-	private static final String ALG_SHA256 = "SHA-256"; // ハッシュ化のアルゴリズム：SHA-256
-	private static final String ALG_SHA512 = "SHA-512"; // ハッシュ化のアルゴリズム：SHA-512
-	
-    /** 
-     * Creates a new instance of <code>EncryptedTextTransform</code>. 
+
+    /** ハッシュ化のアルゴリズム：SHA-1 */
+    private static final String ALG_SHA1 = "SHA-1";
+    /** ハッシュ化のアルゴリズム：SHA-256 */
+    private static final String ALG_SHA256 = "SHA-256";
+    /** ハッシュ化のアルゴリズム：SHA-512 */
+    private static final String ALG_SHA512 = "SHA-512";
+
+    /**
+     * Creates a new instance of <code>EncryptedTextTransform</code>.
      */
     public EncryptedTextTransform() {
     }
-    
+
     /**
      * パスワードのハッシュ化（登録用）
      * @param inputPass
@@ -28,32 +30,46 @@ public class EncryptedTextTransform implements JDBCPasswordSyntaxTransform  {
      * @throws NoSuchAlgorithmException
      */
     public String transform(String inputPass) throws NoSuchAlgorithmException {
-    	
-    	// Salt(4byte)のランダム生成
-    	byte[] bRndSalt = OpenLDAPEncryption.generateSalt();
-    	
-    	// 入力したパスワードのハッシュ化
-    	byte[] hashedPass = OpenLDAPEncryption.encryptionPassword1(ALG_SHA1, inputPass, bRndSalt);
-    	String encPass = OpenLDAPEncryption.encryptionPassword2(ALG_SHA1, hashedPass, bRndSalt);
-    	
-    	return encPass;
+
+        OpenLDAPEncryption clsLDAPEnc = new OpenLDAPEncryption();
+
+        // Saltのランダム生成
+        byte[] bRndSalt = clsLDAPEnc.generateSalt();
+
+        // 入力したパスワードのハッシュ化
+        byte[] hashedPass = clsLDAPEnc.encryptionPassword1(ALG_SHA1, inputPass, bRndSalt);
+        String encPass = clsLDAPEnc.encryptionPassword2(ALG_SHA1, hashedPass, bRndSalt);
+
+        return encPass;
     }
-    
+
     /**
      * パスワードのハッシュ化（ログイン時の検証用）
      * @param extPass
      * @param inputPass
      * @return
-     * @throws NoSuchAlgorithmException 
+     * @throws NoSuchAlgorithmException
      */
     public byte[] transformCompare(String extPass, String inputPass) throws NoSuchAlgorithmException {
-    	
-    	// Saltの抽出
-    	byte[] bExtSalt = OpenLDAPEncryption.extractionSalt(ALG_SHA1, extPass);
-    	
-    	// 入力したパスワードの暗号化
-    	byte[] hashedPass = OpenLDAPEncryption.encryptionPassword1(ALG_SHA1, inputPass, bExtSalt);
-    	
-    	return hashedPass;
+
+        OpenLDAPEncryption clsLDAPEnc = new OpenLDAPEncryption();
+
+        // Saltの抽出
+        byte[] bExtSalt = clsLDAPEnc.extractionSalt(ALG_SHA1, extPass);
+
+        // 入力したパスワードの暗号化
+        byte[] hashedPass = clsLDAPEnc.encryptionPassword1(ALG_SHA1, inputPass, bExtSalt);
+
+        return hashedPass;
+    }
+
+    /**
+     * @param extPass
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
+    public byte[] decryptionPassword(String extPass) throws NoSuchAlgorithmException {
+        OpenLDAPEncryption clsLDAPEnc = new OpenLDAPEncryption();
+        return clsLDAPEnc.decryptionPassword(ALG_SHA1, extPass);
     }
 }
